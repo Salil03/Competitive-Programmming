4e4096cc378e86f8bc9566be0539506f46f95e2a
Salil Gokhale <salilgokhale03@gmail.com>
2018-08-05 12:04:06 +0530

    7

--- C++/Exercises/Chapter 2/Ex 1.cpp
+++ C++/Exercises/Chapter 2/Ex 1.cpp
@@ -1,6 +1,7 @@
 #include "std_lib_facilities.h"
-int main() {
-  cout << "Hello,programming!\nHere we go!\n";
-  keep_window_open();
-  return 0;
+int main()
+{
+    cout << "Hello,programming!\nHere we go!\n";
+    keep_window_open();
+    return 0;
 }
--- C++/Exercises/Chapter 3/Ex 7.cpp
+++ C++/Exercises/Chapter 3/Ex 7.cpp
@@ -1,27 +1,21 @@
 #include "std_lib_facilities.h"
-int main()
-{
+int main() {
   cout << "Please type three integers\n";
   string val1, val2, val3;
   cin >> val1 >> val2 >> val3;
-  if (val1 < val2)
-  {
+  if (val1 < val2) {
     if (val2 < val3)
       cout << val1 << ',' << val2 << ',' << val3 << endl;
-    else
-    {
+    else {
       if (val3 < val1)
         cout << val3 << ',' << val1 << ',' << val2 << endl;
       else
         cout << val1 << ',' << val3 << ',' << val2 << endl;
     }
-  }
-  else
-  {
+  } else {
     if (val1 < val3)
       cout << val2 << ',' << val1 << ',' << val3 << endl;
-    else
-    {
+    else {
       if (val3 < val2)
         cout << val3 << ',' << val2 << ',' << val1 << endl;
       else
