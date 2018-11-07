a8df3475a58c614a2c982147bf42eda7c26b47f1
Salil Gokhale <salilgokhale03@gmail.com>
2018-11-07 12:37:47 +0530

    21

--- Programs/C++/CodeChef/ZCO/SUPW.cpp
+++ Programs/C++/CodeChef/ZCO/SUPW.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 long long work[2000000];
 long long arr[2000000];
-void minutes()
+void minutes(int x)
 {
     arr[0] = 0;
     arr[1] = 0;
     arr[2] = 0;
-    for (long long i = 3; i <= 200050; i++)
+    for (long long i = 3; i <= x; i++)
     {
         arr[i] = min({work[i - 3] + arr[i - 3], work[i - 2] + arr[i - 2], work[i - 1] + arr[i - 1]});
     }
@@ -20,6 +20,6 @@ int main()
     {
         cin >> work[i];
     }
-    minutes();
+    minutes(n);
     cout << arr[n];
 }
