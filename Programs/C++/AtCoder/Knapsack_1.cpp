b88448ed163b789de48c8e60ceef0ca67e4b1b7e
Salil03 <salilgokhale03@gmail.com>
2019-01-24 09:59:10 +0530

    Committed by TeXstudio

--- Programs/C++/AtCoder/Knapsack_1.cpp
+++ Programs/C++/AtCoder/Knapsack_1.cpp
@@ -1,16 +1,36 @@
-#include<bits/stdc++.h>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	int n,w;
-	cin >> n >> w;
-	int weight[n];
-	int value[n];
-	for(int i = 0; i<n; i++)
-	{
-		cin >> weight[i] >> value[i];
-	}
+
+typedef long long ll;
+
+const int MAXN = 105, MAXV = 100005;
+const ll inf = 0x3f3f3f3f3f3f3f3f;
+
+ll dp[MAXV];
+ll weight[MAXN];
+ll value[MAXN];
+int object_count, max_weight;
+
+void solve() {
+    for (int v = 1; v < MAXV; ++v) dp[v] = inf;
+    for (int i = 1; i <= object_count; ++i) {
+        for (int v = MAXV - 1; v >= value[i]; --v) {
+            dp[v] = min(dp[v], dp[v - value[i]] + weight[i]);
+        }
+    }
+}
+
+int main() {
+    scanf("%d%d", &object_count, &max_weight);
+    for (int i = 1; i <= object_count; ++i) {
+        scanf("%lld%lld", weight + i, value + i);
+    }
+    solve();
+    for (int v = MAXV - 1; ~v; --v) {
+        if (dp[v] && dp[v] <= max_weight) {
+            printf("%d\n", v);
+            return 0;
+        }
+    }
 }
--- Programs/C++/test.cpp
+++ Programs/C++/test.cpp
@@ -1,314 +1,218 @@
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
-#include <windows.h>
-#include <vector>
 #include <conio.h>
 
-using namespace std;
+void run();
+void printMap();
+void initMap();
+void move(int dx, int dy);
+void update();
+void changeDirection(char key);
+void clearScreen();
+void generateFood();
 
-class snake {
-private: int x, y,tail,apple_x,apple_y,dx,dy,lastpos_x,lastpos_y,points,level;
-         bool go,eaten;
-         char**map;
+char getMapValue(int value);
 
-//vectors to store the tail of the snake
+// Map dimensions
+const int mapwidth = 20;
+const int mapheight = 20;
 
-         std::vector<int> x_tail;
-         std::vector<int> y_tail;
-public: snake();
-        ~snake();
-        void start_game();
-        void game_over();
-        void move();
-        void input();
-        void apple();
-        void game();
-        void refresh();
-        void clear();
-        void draw();
-        void hidecursor();
-        void test();
-        int check_over();
-};
+const int size = mapwidth * mapheight;
 
-//hide cursor function
+// The tile values for the map
+int map[size];
 
-void snake::hidecursor()
+// Snake head details
+int headxpos;
+int headypos;
+int direction;
+
+// Amount of food the snake has (How long the body is)
+int food = 3;
+
+// Determine if game is running
+bool running;
+
+int main()
 {
-    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO info;
-    info.dwSize = 100;
-    info.bVisible = FALSE;
-    SetConsoleCursorInfo(consoleHandle, &info);
-}
-
-//test function
-
-void snake::test() {
-    for (int i = 0; i <= 5; i++) {
-        x_tail.push_back(i);
-    }
-    cout << endl;
-    x_tail.insert(x_tail.begin(), 100);
-    for (unsigned i = 0; i < x_tail.size(); i++) {
-        cout << x_tail[i] << " ";
-    }
-}
-
-//initial values
-
-snake::snake() {
-    go = true;
-    eaten = false;
-    level = 100;
-    dx = 0;
-    dy = 1;
-    x = 6;
-    y = 25;
-    apple_x = 1;
-    apple_y = 1;
-    tail = 3;
-    points = 0;
-}
-
-snake::~snake() {
-    for (int i = 0; i < 26; i++) {
-        delete[] map[i];
-    }
-    delete[] map;
-}
-
-//start the game
-
-void snake::start_game() {
-    hidecursor();
-    map = new char*[26];
-    for (int i = 0; i < 26; i++) {
-        map[i] = new char[81];
-    }
-    draw();
-}
-
-//draw map
-
-void snake::draw() {
-
-//draw top and bottom walls
-
-    for (int j = 1; j <= 80; j++) {
-            map[1][j] = char(178);
-            map[25][j] = char(178);
-        }
-
-//draw right and left walls
-
-    for (int i = 1; i <= 25; i++) {
-            map[i][1] = char(178);
-            map[i][80] = char(178);
-    }
-    for (int i = 2; i <= 24; i++) {
-        for (int j = 2; j <= 79; j++) {
-            map[i][j] = ' ';
-        }
-    }
-//draw head and tail of the snake
-
-    map[x][y] = char(254);
-    for (int i = 22; i <= 24; i++) {
-        y_tail.push_back(i);
-    }
-    for (int i = 0; i < tail; i++) {
-        x_tail.push_back(x);
-    }
-    for (int i = 0; i < tail; i++) {
-        map[x_tail[i]][y_tail[i]] = 'o';
-    }
-
-//display the entire map
-
-    for (int i = 1; i <= 25; i++) {
-        for (int j = 1; j <= 80; j++) {
-            if (j == 40)
-                cout << map[i][j] << endl;
-            else
-                cout << map[i][j];
-        }
-    }
-
-//generate apple
-    apple();
-
-//movement
-    move();
-}
-
-void snake::refresh() {
-
-//update map with the new positions
-
-    map[x][y] = char(254);
-    for (int i = 0; i < tail; i++) {
-        map[x_tail[i]][y_tail[i]] = 'o';
-    }
-
-//display the updated map
-
-    for (int i = 1; i <= 25; i++) {
-        for (int j = 1; j <= 80; j++) {
-            if (j == 80)
-                cout << map[i][j] << endl;
-            else
-                cout << map[i][j];
-        }
-    }
-
-//continue to detect movement after update
-    move();
-}
-
-//apple random generation
-void snake::apple() {
-    srand((unsigned)time(NULL));
-    apple_x = 2 + rand() % (24 - 2 + 1);
-    apple_y = 2 + rand() % (79 - 2 + 1);
-    map[apple_x][apple_y] = 'o';
-}
-
-//check if the snake hit a wall or himself
-
-int snake::check_over() {
-    if ((x == 1) || (x == 25) || (y == 1) || (y == 80)) {
-        return 1;
-    }
-    for (int i = 0; i < tail-1; i++) {
-        if ((x_tail[i] == x) && (y_tail[i] == y))
-            return 1;
-    }
+    run();
     return 0;
 }
 
-//game over...
+// Main game function
+void run()
+{
+    // Initialize the map
+    initMap();
+    running = true;
+    while (running) {
+        // If a key is pressed
+        if (kbhit()) {
+            // Change to direction determined by key pressed
+            changeDirection(getch());
+        }
+        // Upate the map
+        update();
 
-void snake::game_over() {
-    system("cls");
-    Sleep(1000);
-    cout << "Game Over!!!" << endl;
-    Sleep(50);
-    cout << "You earned " << points << " points!!!" << endl;
+        // Clear the screen
+        clearScreen();
+
+        // Print the map
+        printMap();
+
+        // wait 0.5 seconds
+        _sleep(500);
+    }
+
+    // Print out game over text
+    std::cout << "\t\t!!!Game over!" << std::endl << "\t\tYour score is: " << food;
+
+    // Stop console from closing instantly
+    std::cin.ignore();
 }
 
-void snake::clear() {
-    system("cls");
-}
+// Changes snake direction from input
+void changeDirection(char key) {
+    /*
+      W
+    A + D
+      S
 
-//snake movement
-
-void snake::move() {
-
-//repeat movement until game over
-    while (go == true) {
-
-//update directions of the snake if a key is pressed... if not keep previous directions
-
-        if (_kbhit()) {
-            input();
-        }
-//update head position
-
-        cout << endl;
-        lastpos_x = x;
-        lastpos_y = y;
-        x = x + dx;
-        y = y + dy;
-
-//generate new apple and increase tail if apple is eaten
-        if ((x == apple_x) && (y == apple_y)) {
-            points = points + 10;
-            eaten = true;
-            map[apple_x][apple_y] = ' ';
-            apple();
-        }
-
-        if (eaten == true) {
-            tail++;
-            x_tail.insert(x_tail.begin(), x_tail[0]);
-            y_tail.insert(y_tail.begin(), y_tail [0]);
-            eaten = false;
-        }
-
-//build tail
-
-        map[x_tail[0]][y_tail[0]] = ' ';
-        for (int i = 0; i < tail-1; i++) {
-            x_tail[i] = x_tail[i+1];
-            y_tail[i] = y_tail[i+1];
-        }
-        x_tail[tail - 1] = lastpos_x;
-        y_tail[tail - 1] = lastpos_y;
-        if (check_over()) {
-            game_over();
-            go = false;
-            break;
-        }
-
-//increase velocity depending on the points earned
-
-        if (points > 50 && points <= 100) {
-            level = 75;
-        }
-        else if (points > 100 && points <= 150) {
-            level = 50;
-        }
-        else if (points > 150) {
-            level = 25;
-        }
-        Sleep(level);
-
-//clear the screen and update the map
-
-        clear();
-        refresh();
+      1
+    4 + 2
+      3
+    */
+    switch (key) {
+    case 'w':
+        if (direction != 2) direction = 0;
+        break;
+    case 'd':
+        if (direction != 3) direction = 1;
+        break;
+    case 's':
+        if (direction != 4) direction = 2;
+        break;
+    case 'a':
+        if (direction != 5) direction = 3;
+        break;
     }
 }
 
-//keyboard input
+// Moves snake head to new location
+void move(int dx, int dy) {
+    // determine new head position
+    int newx = headxpos + dx;
+    int newy = headypos + dy;
 
-void snake::input() {
-    if (_kbhit()) {
-        char in = _getch();
-        switch (in) {
-            case 'd':
-            {
-                dx = 0;
-                dy = 1;
-                break;
-            }
-            case 'w':
-            {
-                dx = -1;
-                dy = 0;
-                break;
-            }
-            case 'a':
-            {
-                dx = 0;
-                dy = -1;
-                break;
-            }
-            case 's':
-            {
-                dx = 1;
-                dy = 0;
-                break;
-            }
-        }
+    // Check if there is food at location
+    if (map[newx + newy * mapwidth] == -2) {
+        // Increase food value (body length)
+        food++;
+
+        // Generate new food on map
+        generateFood();
+    }
+
+    // Check location is free
+    else if (map[newx + newy * mapwidth] != 0) {
+        running = false;
+    }
+
+    // Move head to new location
+    headxpos = newx;
+    headypos = newy;
+    map[headxpos + headypos * mapwidth] = food + 1;
+
+}
+
+// Clears screen
+void clearScreen() {
+    // Clear the screen
+    system("cls");
+}
+
+// Generates new food on map
+void generateFood() {
+    int x = 0;
+    int y = 0;
+    do {
+        // Generate random x and y values within the map
+        x = rand() % (mapwidth - 2) + 1;
+        y = rand() % (mapheight - 2) + 1;
+
+        // If location is not free try again
+    } while (map[x + y * mapwidth] != 0);
+
+    // Place new food
+    map[x + y * mapwidth] = -2;
+}
+
+// Updates the map
+void update() {
+    // Move in direction indicated
+    switch (direction) {
+    case 0: move(-1, 0);
+        break;
+    case 1: move(0, 1);
+        break;
+    case 2: move(1, 0);
+        break;
+    case 3: move(0, -1);
+        break;
+    }
+
+    // Reduce snake values on map by 1
+    for (int i = 0; i < size; i++) {
+        if (map[i] > 0) map[i]--;
     }
 }
 
+// Initializes map
+void initMap()
+{
+    // Places the initual head location in middle of map
+    headxpos = mapwidth / 2;
+    headypos = mapheight / 2;
+    map[headxpos + headypos * mapwidth] = 1;
 
-int main() {
-    snake game;
-    game.start_game();
-    system("pause");
- }
+    // Places top and bottom walls
+    for (int x = 0; x < mapwidth; ++x) {
+        map[x] = -1;
+        map[x + (mapheight - 1) * mapwidth] = -1;
+    }
+
+    // Places left and right walls
+    for (int y = 0; y < mapheight; y++) {
+        map[0 + y * mapwidth] = -1;
+        map[(mapwidth - 1) + y * mapwidth] = -1;
+    }
+
+    // Generates first food
+    generateFood();
+}
+
+// Prints the map to console
+void printMap()
+{
+    for (int x = 0; x < mapwidth; ++x) {
+        for (int y = 0; y < mapheight; ++y) {
+            // Prints the value at current x,y location
+            std::cout << getMapValue(map[x + y * mapwidth]);
+        }
+        // Ends the line for next x value
+        std::cout << std::endl;
+    }
+}
+
+// Returns graphical character for display from map value
+char getMapValue(int value)
+{
+    // Returns a part of snake body
+    if (value > 0) return 'o';
+
+    switch (value) {
+        // Return wall
+    case -1: return 'X';
+        // Return food
+    case -2: return 'O';
+    }
+}
