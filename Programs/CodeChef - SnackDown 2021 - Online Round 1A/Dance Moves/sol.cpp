3c0bf2a91497716a5c77816353fe2eaba01ed6a0
Salil <salilgokhale24@gmail.com>
2021-10-24 14:24:22 +0530

    after jee

---
+++ Programs/CodeChef - SnackDown 2021 - Online Round 1A/Dance Moves/sol.cpp
@@ -0,0 +1,59 @@
+// OPTIMIZATIONS
+//#pragma GCC optimize("O3")
+//(UNCOMMENT WHEN HAVING LOTS OF RECURSIONS)
+//#pragma comment(linker, "/stack:200000000")
+//(UNCOMMENT WHEN NEEDED)
+//#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector,fast-math")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,tune=native")
+// OPTIMIZATIONS
+#include "bits/stdc++.h"
+typedef long long ll;
+typedef unsigned long long uu;
+typedef long long int lll;
+typedef unsigned long long int uuu;
+using namespace std;
+
+#define cel(x, y) 1 + ((x - 1) / y)
+const double PI = 3.141592653589793238463;
+const int MOD = 1000000007;
+const long long int INF = 0x3f3f3f3f3f3f3f3f;
+
+/*
+$alil03
+
+URL: url
+
+Solution Begins here
+*/
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    // freopen("file.in", "r", stdin);
+    // freopen("file.out", "w", stdout);
+    lll t;
+    cin >> t;
+    while (t--)
+    {
+        lll x, y;
+        cin >> x >> y;
+        if (y <= x)
+        {
+            cout << x - y;
+        }
+        else
+        {
+            if ((y - x) % 2 == 0)
+            {
+                cout << (y - x) / 2;
+            }
+            else
+            {
+                cout << (y - x + 1) / 2 + 1;
+            }
+        }
+        cout << "\n";
+    }
+}
